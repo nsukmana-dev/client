363c92b6a86ce87a85446625b39bd0d21b6e8d0b
Nandar Sukmana <nsukmana.dev@gmail.com>
2022-11-24 11:11:16 +0700

    page postlist

--- src/PostList.js
+++ src/PostList.js
@@ -1,34 +1,36 @@
-import React from "react";
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const PostList = () => {
-  const [title, setTitle] = useState("");
+  useEffect(() => {
+    fetchPost();
+  }, []);
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
+  const [posts, setPosts] = useState({});
 
-    await axios.post("http://localhost:4000/posts", {
-      title,
-    });
+  const fetchPost = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
 
-    setTitle("");
+    setPosts(res.data);
   };
 
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label>Title</label>
-          <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="form-control"
-          />
+  const renderPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
         </div>
-        <button className="btn btn-primary">Submit</button>
-      </form>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-warp justify-content-between ">
+      {renderPosts}
     </div>
   );
 };

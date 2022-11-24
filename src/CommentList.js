c4ff6826da5ba63adc5e35289c5403672f6f28d4
Nandar Sukmana <nsukmana.dev@gmail.com>
2022-11-24 13:34:20 +0700

    page list comment

--- src/CommentList.js
+++ src/CommentList.js
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const CommentList = ({ postId }) => {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const [comments, setComments] = useState([]);
 
   const fetchData = async () => {
     const res = await axios.get(
       `http://localhost:4001/posts/${postId}/comments`
     );
+
+    setComments(res.data);
   };
-  return <div></div>;
+
+  const renderComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderComments}</ul>;
 };
 
 export default CommentList;
